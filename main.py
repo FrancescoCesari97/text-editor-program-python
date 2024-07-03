50133e2b8c464309a35015f37f8d37d332c6747c
FrancescoCesari97 <freedus97@gmail.com>
2024-07-03 16:11:30 +0200

    defined open file function

--- main.py
+++ main.py
@@ -13,10 +13,26 @@ def change_font(*args):
     text_area.config(font=(font_name.get(), size_box.get()))
 
 def new_file():
-    pass
+    window.title("Utitled")
+    text_area.delete(1.0, END)
 
 def open_file():
-    pass
+    file = askopenfilename(defaultextension=".txt",
+                           file=[("All Files", "*.*"),
+                                  ("Text Documents", "*.txt")])
+    try:
+        window.title(os.path.basename(file))
+        text_area.delete(1.0, END)
+
+        file = open(file, "r")
+
+        text_area.insert(1.0, file.read())
+    
+    except Exception:
+        print("could't read file")
+        
+    finally:
+        file.close()
 
 def save_file():
     pass
